745443751aff5f256fbbdfdc6a3629f8d74a21ed
Welliton Gervickas <wellitogervickas@gmail.com>
2019-07-10 21:12:08 -0300

    Created initial props slider and slider item

--- src/components/Slider/Item.js
+++ src/components/Slider/Item.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Item = () => (
-  <div>Item</div>
-)
+const Item = ({
+  template,
+  image,
+  title,
+  subTitle,
+  description,
+}) => {
+  console.log(
+    template,
+    image,
+    title,
+    subTitle,
+    description,
+  )
+
+  return <div>Item</div>
+}
+
+Item.propTypes = {
+  template: PropTypes.oneOf(['default', 'template_2', 'template_3', 'template_4']),
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string,
+  description: PropTypes.string.isRequired,
+}
+
+Item.defaultProps = {
+  template: 'default',
+  subTitle: null,
+}
 
 export default Item
--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -2,14 +2,29 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Item from './Item'
 
-const Slider = ({ children }) => (
-  <div>
-    {children}
-  </div>
-)
+const Slider = ({
+  duration, effect, autoPlay, children,
+}) => {
+  console.log(duration, effect, autoPlay)
+
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
 
 Slider.propTypes = {
   children: PropTypes.node.isRequired,
+  duration: PropTypes.number,
+  effect: PropTypes.oneOf(['fade-in', 'slide-in-left']),
+  autoPlay: PropTypes.bool,
+}
+
+Slider.defaultProps = {
+  duration: 500,
+  effect: 'slide-in-left',
+  autoPlay: false,
 }
 
 Slider.Item = Item
