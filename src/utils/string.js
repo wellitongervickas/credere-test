ff7dc491df6555345af0f1e40f9c89bf92e69a86
Welliton Gervickas <wellitogervickas@gmail.com>
2019-07-14 17:27:35 -0300

    Created utils text test

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,4 +1,4 @@
-export const cropText = (text, max = 80) => {
+export const cropText = (text = '', max = 80) => {
   if (text.length > max) {
     return `${text.slice(0, max)}...`
   }
---
+++ src/utils/string.test.js
@@ -0,0 +1,30 @@
+import { cropText } from './string'
+
+describe('String Utils', () => {
+  it('should crop text without default props', () => {
+    const string = cropText()
+    expect(string).toBe('')
+    expect(string).toHaveLength(0)
+  })
+
+  it('should crop text with just text props', () => {
+    const text = 'My name is John Cena'
+    const string = cropText(text)
+    expect(string).toBe(text)
+    expect(string).toHaveLength(20)
+  })
+
+  it('should crop text with all props but short text', () => {
+    const text = 'My name is John Cena'
+    const string = cropText(text, 50)
+    expect(string).toBe(text)
+    expect(string).toHaveLength(20)
+  })
+
+  it('should crop text with all props with long text', () => {
+    const cropMax = 20
+    const string = cropText('My name is John Cena and i will destroy you', cropMax)
+    expect(string).toBe('My name is John Cena...')
+    expect(string).toHaveLength(cropMax + 3) // with dots
+  })
+})
