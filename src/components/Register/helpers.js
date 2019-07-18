7bdb2c30a3b4a1140bec76b694c4c83815c7e886
Welliton Gervickas <wellitogervickas@gmail.com>
2019-07-18 15:33:14 -0300

    Create Register Helpers Tests and Snapshots

--- src/components/Register/helpers.js
+++ src/components/Register/helpers.js
@@ -1,22 +1,28 @@
 export const isUnderAge = (date) => {
-  const currentYear = new Date().getFullYear()
-  const ageYear = new Date(date).getFullYear()
+  if (date) {
+    const currentYear = new Date().getFullYear()
+    const ageYear = new Date(date).getFullYear()
 
-  return currentYear - ageYear < 18
+    return currentYear - ageYear < 18
+  }
+
+  return true
 }
 
-export const showCity = (state = '', driveLicense = 0) => {
-  const sanitizedState = state.toLowerCase()
-  const initialDriveLicense = driveLicense.slice(0, 1)
+export const showCity = (state, driveLicense) => {
+  if (state && driveLicense >= 0) {
+    const sanitizedState = state.toLowerCase()
+    const initialDriveLicense = driveLicense.slice(0, 1)
 
-  if (sanitizedState === 'rn' && initialDriveLicense === '6') {
-    return true
+    if (sanitizedState === 'rn' && initialDriveLicense === '6') {
+      return true
+    }
   }
 
   return false
 }
 
-export const getFieldState = (fields, field) => {
+export const getFieldValue = (fields = [], field) => {
   const el = fields.find(item => item.key === field)
   return el ? el.value : ''
 }
--- src/components/Register/index.test.js
+++ src/components/Register/index.test.js
@@ -1,10 +1,112 @@
 import React from 'react'
+import { mount } from 'enzyme'
 import renderer from 'react-test-renderer'
 import Register from '.'
+import * as helpers from './helpers'
+import ParentFields from './ParentFields'
+import DriverLicense from './DriverLicense'
+import EmailsField from './Emails'
+import PhonesField from './Phones'
+import DetailsField from './Details'
+import AddressField from './Address'
 
 describe('Register Component', () => {
   it('should render register component', () => {
     const element = renderer.create(<Register />).toJSON()
     expect(element).toMatchSnapshot()
+  })
+
+  it('should render partially components of Register', () => {
+    const tree = mount(
+      <>
+        <ParentFields />
+        <DriverLicense />
+        <EmailsField />
+        <PhonesField />
+        <DetailsField />
+        <AddressField />
+      </>,
+    )
+
+    expect(tree).toMatchSnapshot()
+  })
+})
+
+describe('Register Helpers', () => {
+  it('should function isUnderAge to be defined', () => {
+    expect(helpers.isUnderAge()).toBeDefined()
+  })
+
+  it('should user is under age', () => {
+    const userDate = '12/12/2002'
+    expect(helpers.isUnderAge(userDate)).toBeTruthy()
+  })
+
+  it('should user is of age', () => {
+    const userDate = '12/12/1999'
+    expect(helpers.isUnderAge(userDate)).toBeFalsy()
+  })
+
+  it('should user is under age because dont have props', () => {
+    expect(helpers.isUnderAge()).toBeTruthy()
+  })
+
+  it('should function showCity to be defined', () => {
+    expect(helpers.showCity()).toBeDefined()
+  })
+
+  it('should dont show city field because dont have props', () => {
+    const isToShowCity = helpers.showCity()
+    expect(isToShowCity).toBeFalsy()
+  })
+
+  it('should dont show city field because dont have drive license prop', () => {
+    const isToShowCity = helpers.showCity('RN')
+    expect(isToShowCity).toBeFalsy()
+  })
+
+  it('should dont show city field because dont have state prop', () => {
+    const isToShowCity = helpers.showCity('', '6543312')
+    expect(isToShowCity).toBeFalsy()
+  })
+
+  it('should dont show city field because dont have drive license initial value 6', () => {
+    const isToShowCity = helpers.showCity('RN', '123456')
+    expect(isToShowCity).toBeFalsy()
+  })
+
+  it('should dont show city field because state field isnt RN', () => {
+    const isToShowCity = helpers.showCity('PR', '654321')
+    expect(isToShowCity).toBeFalsy()
+  })
+
+  it('should show city because field state is RN and initial value of drive license is 6', () => {
+    const isToShowCity = helpers.showCity('RN', '654321')
+    expect(isToShowCity).toBeTruthy()
+  })
+
+  it('should function getFieldValue to be defined', () => {
+    expect(helpers.getFieldValue()).toBeDefined()
+  })
+
+  it('should return empty value because dont have fields and fields and field is null', () => {
+    const fields = []
+    const field = null
+    const expected = helpers.getFieldValue(fields, field)
+    expect(expected).toEqual('')
+  })
+
+  it('should return empty value because cant find a password field in fields', () => {
+    const fields = [{ key: 'name', value: 'test' }]
+    const field = 'password'
+    const expected = helpers.getFieldValue(fields, field)
+    expect(expected).toEqual('')
+  })
+
+  it('should return a valid value because can find field in fields list', () => {
+    const fields = [{ key: 'name', value: 'test' }]
+    const field = 'name'
+    const expected = helpers.getFieldValue(fields, field)
+    expect(expected).toEqual('test')
   })
 })
